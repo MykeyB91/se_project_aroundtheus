3bc8113e7fed300e2cce3b7900eb06cb04b38254
MBell91 <Mykalbell@ymail.com>
2024-07-18 22:54:06 -0400

    Update index.css

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,1 +1,17 @@
-console.log("hello from validation.js");
+// enabling validation by calling enableValidation()
+// pass all the settings on call
+
+function enableValidation(config) {
+  console.log(config);
+}
+
+const config = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__button",
+  inactiveButtonClass: ".modal__button_disabled",
+  inputErrorClass: ".modal__input_type_error",
+  errorClass: ".modal__error_visible",
+};
+
+//enableValidation(config);
