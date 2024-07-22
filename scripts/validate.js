6ed70426f98f3837ff7b8b520192a5af07dc9013
MBell91 <Mykalbell@ymail.com>
2024-07-22 00:16:55 -0400

    Update validate.js and modal.css

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,8 +1,68 @@
 // enabling validation by calling enableValidation()
 // pass all the settings on call
 
-function enableValidation(config) {
-  console.log(config);
+function hideInputError(formEl, inputEl, { inputErrorClass }) {
+  const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
+  inputEl.classList.remove(inputErrorClass);
+  errorMessageEl.textContent = "";
+  errorMessageEl.classList.remove(inputErrorClass);
+  //console.log(`#${inputEl.id}-error`);
+}
+
+function showInputError(formEl, inputEl, { inputErrorClass }) {
+  const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
+  inputEl.classList.add(inputErrorClass);
+  errorMessageEl.textContent = inputEl.validationMessage;
+  errorMessageEl.classList.add(errorClass);
+  //console.log(`#${inputEl.id}-error`);
+}
+
+function checkInputValidity(formEl, inputEl, options) {
+  if (!inputEl.validity.valid) {
+    showInputError(formEl, inputEl, options);
+  } else {
+    hideInputError(formEl, inputEl, options);
+  }
+}
+
+function toggleButtonState(inputElms, submitButton, { inactiveButtonClass }) {
+  const foundInvalid = false;
+
+  inputElms.forEach((inputEl) => {
+    if (!inputEl.validity.valid) {
+      foundInvalid = true;
+    }
+  });
+
+  if (foundInvalid) {
+    submitButton.classList.add(inactiveButtonClass);
+    submitButton.disabled = true;
+  } else {
+    submitButton.classList.remove(inactiveButtonClass);
+    submitButton.disabled = false;
+  }
+}
+
+function setEventListener(formEl, options) {
+  const { inputSelector } = options;
+  const inputElms = [...formEl.querySelectorAll(inputSelector)];
+  inputElms.forEach((inputEl) => {
+    inputEl.addEventListener("input", (e) => {
+      checkInputValidity(formEl, inputEl, options);
+      toggleButtonState(inputElms, submitButton, options);
+    });
+  });
+}
+
+function enableValidation(options) {
+  const formElms = [...document.querySelectorAll(options.formSelector)];
+  formElms.forEach((formEl) => {
+    formEl.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+
+    setEventListener(formEl, options);
+  });
 }
 
 const config = {
@@ -14,4 +74,4 @@ const config = {
   errorClass: ".modal__error_visible",
 };
 
-//enableValidation(config);
+enableValidation(config);
